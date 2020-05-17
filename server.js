cec1538ac3443c2b91250172f0e278d016768530
chudotvorojok <ivan.burin@bk.ru>
2020-05-17 21:12:30 +0300

    heroku test #4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(favicon(`${__dirname}/build/favicon.ico`));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', (req, res) => res.send('pong'));
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port); 
